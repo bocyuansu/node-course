81539b7fc0f84dd7a566ccd04d525f2ce5d620f1
Cyuan <j446214@gmail.com>
2022-05-08 14:27:38 +0800

    add sum.js

--- basic/hello.js
+++ basic/hello.js
@@ -1,1 +1,15 @@
 console.log("hello nodejs");
+
+let input = [
+    { id: 1, title: 'AAAA', price: 100, count: 2},
+    { id: 2, title: 'BBBB', price: 300, count: 1},
+    { id: 3, title: 'CCCC', price: 200, count: 1},
+    { id: 4, title: 'DDDD', price: 500, count: 2},
+];
+
+let output = input.reduce((acc , item) => {
+    acc[item.id] = item.price;
+    return acc;
+},{});
+
+console.log(output);
---
+++ basic/sum.js
@@ -0,0 +1,8 @@
+// 回傳 1 + 2 + ... + n 的結果
+
+function sum(n) {
+    return (1 + n) * n / 2;
+    // return 結果
+};
+
+console.log(sum(100));
