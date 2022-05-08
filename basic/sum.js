887650ddf05f9e8b8d8dd0baa1ec772f79c5167a
Cyuan <j446214@gmail.com>
2022-05-08 14:41:13 +0800

    add coding style

--- basic/sum.js
+++ basic/sum.js
@@ -1,9 +1,9 @@
 // 回傳 1 + 2 + ... + n 的結果
 
 function sum(n) {
-    return (1 + n) * n / 2;
-    // return 結果
-};
+  return ((1 + n) * n) / 2;
+  // return 結果
+}
 
 console.log(sum(1)); // 1
 console.log(sum(2)); // 3
