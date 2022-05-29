e397b08f40bcd7f0f1c1babcbc7ab28981c2bb3b
Cyuan <j446214@gmail.com>
2022-05-29 15:20:38 +0800

    add simple-express

--- simple-express/server.js
+++ simple-express/server.js
@@ -47,6 +47,10 @@ app.set('views', path.join(__dirname, 'views'));
 // npm i pug
 app.set('view engine', 'pug');
 
+// extended: false --> querystring
+// extended: true --> qs
+app.use(express.urlencoded({ extended: true }));
+
 // express 處理靜態資料
 // 靜態資料: html, css 檔案, javascript 檔案, 圖片, 影音檔...
 // express 少數內建的中間件 static
@@ -121,20 +125,68 @@ app.get('/stocks', async (req, res, next) => {
 
 // 取得某個股票 id 的資料
 app.get('/stocks/:stockId', async (req, res, next) => {
+  // :stockId 代表設定 stockId 參數
   // 取得網址上的參數 req.params
   // req.params.stockId
   console.log('get stocks by id', req.params);
-  let [data, fields] = await pool.execute('SELECT * FROM stocks WHERE id =' + req.params.stockId);
-  console.log('query stock by id:', data);
+  // pool.execute 可以防止 SQL injection
+  // console.log('query stock by id:', data);
+
+  // RESTful 風格之下，鼓勵把這種過濾參數用 query string 來傳遞
+  // /stocks/:stockId?page=1&year=2022
+  // req.query = {page:1, year:2022}
+  // 1. 取得目前在第幾頁，而且利用 || 這個特性來做預設值
+  // req.query = {}
+  // 如果網址上沒有 page 這個 query string，那 req.query.page 會是 undefined
+  // undefined 會是 false，所以 page 就被設定成 || 後面的數字
+  // 透過 ? 自訂的變數，可以用 req.query.變數名稱，來取得變數的值
+
+  let page = req.query.page || 1; // 如果沒有宣告 page，則預設 page = 1
+  console.log('current page', page);
+
+  // 2. 取得目前的總筆數
+  let [allResults, fields] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id = ?', [req.params.stockId]);
+  const total = allResults.length;
+  console.log('total:', total);
+
+  // 3. 計算總共有幾頁
+  // Math.ceil 1.1 => 2   1.05 -> 2
+  const perPage = 5; // 每一頁有幾筆
+  const lastPage = Math.ceil(total / perPage); // 最後一頁是第幾頁
+  console.log('lastPage:', lastPage);
+
+  // 4. 計算 offset 是多少 (計算要跳過幾筆)
+  // 在第五頁，就是要跳過 4 * perPage
+  let offset = (page - 1) * perPage;
+  console.log('offset:', offset);
+
+  // 5. 取得這一頁的資料 select * from table limit ? offset ?
+  let [pageResults] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id = ? ORDER BY date DESC LIMIT ? OFFSET ?', [req.params.stockId, perPage, offset]);
+
+  // test case
+  // 正面: 沒有page, page=1, page=2, page=12 (因為總共12頁)
+  // 負面: page = -1, page=13, page=空白(page=1), page=a,...
+  // 6. 回覆給前端
+  res.json({
+    // 用來儲存所有跟頁碼有關的資訊
+    pagination: {
+      total,
+      lastPage,
+      page
+    },
+    // 真正的資料
+    data: pageResults,
+  })
+
   // 空資料(查不到資料)，有兩種處理方式：
   // 1. 200 OK 就會 [] ;
   // 2. 回覆 404
-  if(data.length === 0) {
-    // 這裡是 404 的範例
-    res.status(404).json(data);
-  } else {
-    res.json(data);
-  }
+  // if(data.length === 0) {
+  //   // 這裡是 404 的範例
+  //   res.status(404).json(data);
+  // } else {
+  //   res.json(data);
+  // }
 })
 
 // 這個中間件在所有路由的後面
